328442c040778c10d80732b8616e789fc4ef69a6
Rafael <eduardo.suarez@amf.edu.br>
2025-03-15 21:31:20 -0300

    Create ExCavalos.py

---
+++ Semana01/ExCavalos.py
@@ -0,0 +1,5 @@
+#faça um algoritmo que solicite o número de cavalos e apresente o valor total
+cavalos = int(input("numero de cavalos:?" ))
+ferradura = cavalos * 4
+valor = ferradura * 80
+print("valor total", valor )
