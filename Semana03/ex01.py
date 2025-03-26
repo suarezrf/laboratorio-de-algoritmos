b161c51991aad663b64e5a363d0d18584d0597eb
Rafael <eduardo.suarez@amf.edu.br>
2025-03-25 22:25:07 -0300

    Update ex01.py

--- Semana03/ex01.py
+++ Semana03/ex01.py
@@ -1,1 +1,11 @@
 
+nome = input("digite o seu nome:")
+tempo = int(input("digite a quantos anos você trabalha na empresa:"))
+salario = float(input("digite o seu salario:"))
+if tempo >= 5 and salario < 2000:
+    salariof = salario * 1.10
+    print("parabens você merece 10 porento de aumento no seu ",salariof , nome)
+else:
+    salariof = salario * 1.05
+    print("parabens você merece 5 porento de aumento no seu ",salariof , nome)
+
