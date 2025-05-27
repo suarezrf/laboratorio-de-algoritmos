35ac27ffd75dbe9981d4b25f93e8979208ac5180
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2025-05-27 20:13:45 -0300

    Create ex02.py

---
+++ semana 13.5/ex02.py
@@ -0,0 +1,15 @@
+def soma_naturais(n):
+    soma = 0
+    i = 1
+    while i <= n:
+        soma += i
+        i += 1
+    return soma
+
+def main():
+    numero = int(input("Digite um número inteiro positivo: "))
+    if numero < 1:
+        print("Digite um número natural maior que zero.")
+    else:
+        resultado = soma_naturais(numero)
+        print(f"A soma dos números naturais até {numero} é {resultado}")
