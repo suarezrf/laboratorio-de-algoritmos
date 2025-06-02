b81bddf89f8ab7d389800e9e3d71f320350cfb76
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2025-06-02 17:41:42 -0300

    Create ex02.py

---
+++ semana14/ex02.py
@@ -0,0 +1,18 @@
+def multi(n1):
+    n2 = n1 * 2
+    return n2
+
+
+
+def numero():
+    n1 = int(input("digite um numero: "))
+    return n1
+
+
+def main():
+    n1 = numero()
+    n2 = multi(n1)
+    print(n2)
+
+
+main()
